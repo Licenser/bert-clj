b49c4bbffc4d9ebc0cab48166a1df61a01eb0e3d
Heinz N. Gies <heinz@licenser.net>
2012-10-23 04:58:52 +0200

    Fixed project.clj.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject bert-clj "1.1.0-SNAPSHOT"
+(defproject org.clojars.heinz/bert-clj "1.1.0-SNAPSHOT"
   :description "BERT Clojure"
-  :url "http://project-fifo.net"
-  :license {:name "Common Development and Distribution License"
-            :url "http://opensource.org/licenses/CDDL-1.0"}
+  :url "https://github.com/trotter/bert-clj"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [
 	[org.clojure/clojure "1.3.0"]
 	])
