e9874dd868bd8fe8df8e9b41123fb8a80d58e0d4
Heinz N. Gies <heinz@licenser.net>
2012-10-23 04:38:59 +0200

    leinigified.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject bert-clj "1.1.0-SNAPSHOT"
+  :description "BERT Clojure"
+  :url "http://project-fifo.net"
+  :license {:name "Common Development and Distribution License"
+            :url "http://opensource.org/licenses/CDDL-1.0"}
+  :dependencies [
+	[org.clojure/clojure "1.3.0"]
+	])
